3e4f37c43f9eb06787f88b9ca7006b2e08d8ec82
Abdelrhman <abdo612200320@gmail.com>
2025-05-04 14:12:49 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Header from "./components/Header/Header";
+import Category from "./components/Category/Category";
+import ItemsList from "./components/Cartitems/ItemsList";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Container>
           <NavBar />
           <Header />
+          <Category />
+          <ItemsList />
         </Container>
       </div>
     </>
